ef10081ffc574a8334b369eb98d72154afcfc5e3
Ersin KOÇ <ersinkoc@gmail.com>
2025-12-27 22:37:36 +0200

    Simplify tsup config by consolidating multiple entry points into single configuration object

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,28 +1,16 @@
 import { defineConfig } from 'tsup'
 
-export default defineConfig([
-  // Main entry
-  {
-    entry: ['src/index.ts'],
-    format: ['esm', 'cjs'],
-    dts: true,
-    sourcemap: true,
-    clean: true,
-    external: ['react', 'react-dom'],
-    treeshake: true,
-    minify: false,
-    splitting: false,
+export default defineConfig({
+  entry: {
+    'index': 'src/index.ts',
+    'plugins/index': 'src/plugins/index.ts',
   },
-  // Plugins entry
-  {
-    entry: ['src/plugins/index.ts'],
-    outDir: 'dist/plugins',
-    format: ['esm', 'cjs'],
-    dts: true,
-    sourcemap: true,
-    external: ['react', 'react-dom'],
-    treeshake: true,
-    minify: false,
-    splitting: false,
-  },
-])
+  format: ['esm', 'cjs'],
+  dts: true,
+  sourcemap: true,
+  clean: true,
+  external: ['react', 'react-dom'],
+  treeshake: true,
+  minify: false,
+  splitting: false,
+})
